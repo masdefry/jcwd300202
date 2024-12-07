1c4087f644b13b02340ef72aaab21e0d9b6a0246
Muhamad Fauzi Aviawan <mfauziavi@gmail.com>
2024-12-07 16:44:52 +0700

    feat: create footer and page responsiveness

--- apps/web/src/features/home/components/CityRecommendationCard.tsx
+++ apps/web/src/features/home/components/CityRecommendationCard.tsx
@@ -24,11 +24,15 @@ const CityRecommendationCard = ({ imgSrc, alt, city, country, h1Size }: ICityRec
               className='object-cover w-full h-full'
               />
             </figure>
-            <div className='z-10 rounded-md bg-black bg-opacity-35 transition duration-200 hover:bg-opacity-0 absolute top-0 left-0 w-full h-full'></div>
+            <div className='z-10 rounded-md bg-black bg-opacity-50 lg:bg-opacity-35 transition duration-200 hover:bg-opacity-0 absolute top-0 left-0 w-full h-full'></div>
+            <hgroup className='lg:hidden absolute bottom-4 left-4 z-20 flex flex-col gap-1 items-start'>
+              <h1 className={`${h1Size} font-bold text-white`}>{city}</h1>
+              <h6 className='text-sm font-light text-white'>{country}</h6>
+            </hgroup>
           </div>
-          <hgroup className='flex flex-col gap-1 items-start z-10'>
+          <hgroup className='hidden lg:flex flex-col gap-1 items-start z-10'>
             <h1 className={`${h1Size} font-bold text-black`}>{city}</h1>
-            <h6 className='text-xl font-light text-black'>{country}</h6>
+            <h6 className='text-xl font-light text-gray-600'>{country}</h6>
           </hgroup>
     </div>
   )

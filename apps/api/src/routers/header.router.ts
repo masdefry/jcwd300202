5e92f49286d3274b2a47d35eed9907953fb26057
Muhamad Fauzi Aviawan <mfauziavi@gmail.com>
2024-12-12 09:45:46 +0700

    feat: create controller dropdown search

--- apps/api/src/routers/header.router.ts
+++ apps/api/src/routers/header.router.ts
@@ -1,8 +1,9 @@
-import { fetchData } from '@/controllers/header.controller'
+import { getCityAndCountryList, fetchData } from '@/controllers/header.controller'
 import { Router } from 'express'
 
 const headerRouter = Router()
 
-headerRouter.post('/search', fetchData)
+headerRouter.get('/search', fetchData)
+headerRouter.get('/search/dropdown', getCityAndCountryList)
 
 export default headerRouter

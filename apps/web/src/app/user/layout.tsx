3d1bd84bb18c7bf689a8ce64eae95552cbe94e85
Muhamad Fauzi Aviawan <mfauziavi@gmail.com>
2024-12-21 14:38:10 +0700

    feat: create sidebar ui for user profile

---
+++ apps/web/src/app/user/layout.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+import { MdOutlineManageAccounts, MdVerified } from 'react-icons/md'
+import { GrNotes } from "react-icons/gr";
+import { FaRegListAlt } from 'react-icons/fa';
+import { TbNotification } from 'react-icons/tb';
+import { IoMdLogOut } from 'react-icons/io';
+
+const ProfileUserLayout = ({  children }: { children: ReactNode }) => {
+  return (
+    <main className='w-full min-h-min py-5'>
+      <section className='m-auto max-w-screen-xl grid grid-cols-4  gap-5 w-full h-full'>
+        <section className='shadow-md rounded-md'>
+          <div>
+            <section className='border-b border-slate-300 flex flex-col gap-3 p-5'>
+              <div className='flex items-center gap-5'>
+                <figure className='rounded-full h-16 w-16 bg-blue-300 border-2 border-slate-200 shadow-md'>
+
+                </figure>
+                <hgroup className='flex flex-col'>
+                  <h1 className='text-base font-bold text-gray-800'>Muhamad Fauzi Aviawan</h1>
+                  <div className='flex items-center gap-2 text-xs font-semibold text-gray-400'>
+                    <MdVerified className='text-blue-600' size={13}/>
+                    <p>Verified</p>
+                  </div>
+                  <p className='text-sm font-normal text-gray-400'>Google</p>
+                </hgroup>
+              </div>
+              <div className='rounded-md px-5 py-3 text-white bg-gray-900 text-sm font-bold flex items-center gap-2'>
+              <p>Rent Smarter, Live Better.</p>
+              </div>
+            </section>
+            <section className='border-b border-slate-300 flex flex-col p-5'>
+              <div className='text-base font-semibold text-gray-800 transition duration-100 px-5 py-3 flex items-center gap-2 rounded-md hover:cursor-pointer hover:bg-slate-200'>
+                <MdOutlineManageAccounts size={20} className='text-blue-400'/>
+                Account
+              </div>
+              <div className='text-base font-semibold text-gray-800 transition duration-100 px-5 py-3 flex items-center gap-2 rounded-md hover:cursor-pointer hover:bg-slate-200'>
+                <GrNotes size={20} className='text-blue-400'/>
+                My Order
+              </div>
+              <div className='text-base font-semibold text-gray-800 transition duration-100 px-5 py-3 flex items-center gap-2 rounded-md hover:cursor-pointer hover:bg-slate-200'>
+                <FaRegListAlt size={20} className='text-blue-400'/>
+                Purchase List
+              </div>
+              <div className='text-base font-semibold text-gray-800 transition duration-100 px-5 py-3 flex items-center gap-2 rounded-md hover:cursor-pointer hover:bg-slate-200'>
+                <TbNotification size={20} className='text-blue-400'/>
+                Notification
+              </div>
+              <div className='text-base font-semibold text-gray-800 transition duration-100 px-5 py-3 flex items-center gap-2 rounded-md hover:cursor-pointer hover:bg-slate-200'>
+                <IoMdLogOut size={20} className='text-red-600'/>
+                Sign Out
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className='col-span-3'>
+          {children}
+        </section>
+      </section>
+    </main>
+  )
+}
+
+export default ProfileUserLayout
